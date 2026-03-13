7a7ec9db130e64d914f993edbebdf91b041886b5
andrew <andrewfire@gmail.com>
2026-03-13 17:59:01 +0300

    first commit

--- Source/AViewController.m
+++ Source/AViewController.m
@@ -4,20 +4,18 @@
 //  Created by Devin Ross on 2/4/15.
 //
 //
-
+ 
 #import "AViewController.h"
-
+ 
 @interface AViewController ()
-
+ 
 @end
-
+ 
 @implementation AViewController
-
+ 
 - (void) viewDidLoad{
     [super viewDidLoad];
-    
-    
-    POPSpringAnimation *animate;
 }
-
+ 
 @end
+ 
--- Source/Prefix-Header.h
+++ Source/Prefix-Header.h
@@ -5,16 +5,13 @@
 //  Created by Devin Ross on 2/5/15.
 //
 //
-
+ 
 #ifndef Blank_Prefix_Header_h
 #define Blank_Prefix_Header_h
-
+ 
 @import Foundation;
 @import UIKit;
 @import QuartzCore;
-
-@import curry;
-@import pop;
-@import curryfire;
-
+ 
 #endif
+ 
